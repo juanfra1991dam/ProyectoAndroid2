08105fc731c65ae4c800eef2198d28c5677830d4
juanfra1991 <1743989@alu.murciaeduca.es>
2025-02-27 21:54:09 +0100

    Se modifica el PilotosViewModel quitando metodos sin uso

--- app/src/main/java/com/example/proyectoandroid2/viewmodels/PilotosViewModel.kt
+++ app/src/main/java/com/example/proyectoandroid2/viewmodels/PilotosViewModel.kt
@@ -2,11 +2,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.proyectoandroid2.items.Item
-import com.google.firebase.firestore.FirebaseFirestore
 
 class PilotosViewModel : ViewModel() {
-
-    private val db = FirebaseFirestore.getInstance()
 
     private val _pilotosList = MutableLiveData<List<Item>>()
     val pilotosList: LiveData<List<Item>> get() = _pilotosList
@@ -36,24 +33,5 @@ class PilotosViewModel : ViewModel() {
             item.nombrePiloto.contains(query, ignoreCase = true) || item.numero.toString().contains(query)
         }
         _pilotosList.value = filteredList
-    }
-
-    // Función para alternar el estado de 'favorito' de un piloto
-    fun toggleFavorito(item: Item, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
-        // Cambiar el estado de 'favorito' del item
-        item.favorito = !item.favorito
-
-        // Actualizar el estado en Firestore
-        db.collection("pilotos")
-            .document(item.posicion.toString())
-            .update("favorito", item.favorito)
-            .addOnSuccessListener {
-                // Llamar al callback de éxito
-                onSuccess()
-            }
-            .addOnFailureListener { exception ->
-                // Llamar al callback de error
-                onFailure(exception)
-            }
     }
 }
